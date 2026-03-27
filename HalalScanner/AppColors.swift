3efb0f809df0cd758d3b9ba2a3e2f2cb42df7a3f
yeeerosh <240103130@sdu.edu.kz>
2026-03-28 01:38:15 +0500

    create extension for App Colors, define usage colors

---
+++ HalalScanner/AppColors.swift
@@ -0,0 +1,18 @@
+//
+//  AppColors.swift
+//  HalalScanner
+//
+//  Created by Yerasyl Toleubek on 28.03.2026.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    static let appGreen = UIColor(red: 0.11, green: 0.62, blue: 0.46, alpha: 1)
+    static let appRed = UIColor(red: 205/255, green: 35/255, blue: 35/255, alpha: 1)
+    static let appBackground = UIColor.systemGray6
+    static let appCardGreen = UIColor(red: 0.88, green: 0.95, blue: 0.87, alpha: 1)
+    static let appCardRed = UIColor(red: 0.99, green: 0.92, blue: 0.92, alpha: 1)
+
+}
