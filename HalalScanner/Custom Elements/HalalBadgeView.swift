33086b5f6ec79ceb54d28495d22722bdb4b5f0b1
yeeerosh <240103130@sdu.edu.kz>
2026-04-02 17:53:15 +0500

    for test remove keys for hints

---
+++ HalalScanner/Custom Elements/HalalBadgeView.swift
@@ -0,0 +1,8 @@
+//
+//  HalalBadgeView.swift
+//  HalalScanner
+//
+//  Created by Yerasyl Toleubek on 02.04.2026.
+//
+
+import Foundation
---
+++ HalalScanner/Custom Elements/PaddingLabel.swift
@@ -0,0 +1,8 @@
+//
+//  PaddingLabel.swift
+//  HalalScanner
+//
+//  Created by Yerasyl Toleubek on 02.04.2026.
+//
+
+import Foundation
